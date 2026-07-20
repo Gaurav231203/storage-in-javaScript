97f5930814993564243bbbf402644af19b7de0bb
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-20 13:29:30 +0530

    Added new files

---
+++ localStorage-and-Session-Storage.js
@@ -0,0 +1,12 @@
+//localStorage ----> it us an builting web browser  api which stores data as values and key parirs permanantily  on a browser untill it is manually deleted
+
+
+//Sesson Storage---> it is also a builtin web browser api which also stores the data as a key value pairs but until the website is  closed  if the website is closed the data will be deleted (it is also called a tabs temprory memeory)
+
+
+// Cookies is also an storage which stores the key information in small storage of website properties called cookie property . it is used to remeber User information and  send it to the server automatily with every request.
+
+//store kese krte hai --> setItem("key name ", "key value");
+//data fetch kese krte hai-->getItem("key name");
+//remove kese krte hai-->.removeItem("key name");
+// update kese krte hai-->setItem("keyname","key value");
---
+++ localStoragePractice.1.js
@@ -0,0 +1,4 @@
+localStorage.setItem("name","gaurav" );
+let val=localStorage.getItem("name")
+localStorage.removeItem("name");
+localStorage.setItem("name","gaurii" );
