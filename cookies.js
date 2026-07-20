6b2be33e39e34ba8cbea67e24ccb5a0249481b52
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-20 17:09:49 +0530

    Added new files

---
+++ cookies.js
@@ -0,0 +1,10 @@
+//cookies
+//cookies browser  mai small data save krrne kay liye use hota  hai jo ki under 4kb hota hai;
+//but in  local storage and session storage we store up to 5mb of data;
+
+
+
+
+document.cookie= "email = bishtgaurav22@gmail.com";
+document.cookie= "age= 22";
+document.cookie= "name =gaurav";
