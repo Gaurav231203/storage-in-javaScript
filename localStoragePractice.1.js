6a4cb23cfc15e69bd71dc2dc2fae018bf9c4b847
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-07-20 19:13:38 +0530

    Added new files

--- localStoragePractice.1.js
+++ localStoragePractice.1.js
@@ -1,5 +1,16 @@
+/*
+
+
 localStorage.setItem("name","gaurav" );
 let val=localStorage.getItem("name")
 localStorage.removeItem("name");
 localStorage.setItem("name","gaurii" );
 localStorage.clear();
+*/
+
+/*local storage mai hum bss string store krr sakte hai kuch orrnahi jes e array and object;
+but usko karne ka tareeka hai JSON.stringify use krr kay*/
+
+localStorage.setItem("names" ,JSON.stringify (["gaurav","SubmitEvent","dheeraj" ,"sahil" ,"manish"]));
+let final= JSON.parse(localStorage.getItem("names"));
+console.log(final);
